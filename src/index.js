4237bbf88699366298f2d46fde3cf1ef932f8ab7
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-04-01 15:15:37 +0300

    svg + npm

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "../src/components/App/App";
 import reportWebVitals from "./reportWebVitals";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store, persistor } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    {/* <BrowserRouter basename="/front-end-so-yummy"> */}
     <App />
+    {/* </BrowserRouter> */}
+    {/* </PersistGate> */}
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
